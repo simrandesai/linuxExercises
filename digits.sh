36acd8c897c84679a54ecf5ba98a82d141c9ed8d
Simran Desai <sadesai3@wisc.edu>
2024-02-29 11:51:46 -0600

    implemented digits.sh

--- digits.sh
+++ digits.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-#Write digits.sh, to find the sum of the numbers between 1000 and 2000 (inclusive) having digits only from the set {0, 1}.
+sum=0
 
-#Hint: Use a brace expansion to make the range of numbers, a loop to check each one, and a conditional statement including a regular expression to see whether the digits are in {0, 1}).
+for number in {1000..2000}; do
 
-#Hint: In emacs, run M-x sh-mode to get help with code formatting including indenting.
+    if [[ "$number" =~ ^[01]+$ ]]; then    #checking if numbers are from 0 and 1 set
+        ((sum += number))
+    fi
+done
 
-total_sum = 0
-
-
-for i in $(seq 1000 2000)
-
-do 
+echo "Sum of numbers with digits only from {0, 1} between 1000 and 2000 is: $sum"
