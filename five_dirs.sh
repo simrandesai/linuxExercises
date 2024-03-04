f9a412dec30240b111bdd45ba7b78dc119ae563a
Lexi Siegel <avsiegel@wisc.edu>
2024-03-04 12:10:39 -0600

    added five

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+mkdir five
+cd five || exit 1
+
+for i in {1..5}; do
+    mkdir "dir$i" || exit 1
+    for ((j = 1; j <= 4; j++)); do
+        content=""
+        for ((k = 1; k <= j; k++)); do
+            content="$content$j"$'\n'
+        done
+	content="${content%$'\n'}"
+        echo -e "$content" > "dir$i/file$j" || exit 1
+    done
+done
+
