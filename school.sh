daad475666438bf6593c10d6da293c51318ebe27
Aurora Toutant <atoutant@wisc.edu>
2024-03-04 19:38:00 -0600

    changes to school.sh

--- school.sh
+++ school.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
+cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d ',' -f7 |
+    {
+	sum=0
+	count=0
+	while IFS= read -r value; do
+	    sum=$((sum + value))
+	    count=$((count + 1))
+	done
+	average=$((sum / count))
 
+	echo "Sum of TotalAssessedValue: $sum"
+	echo "Average TotalAssessedValue: $average"
+	}
